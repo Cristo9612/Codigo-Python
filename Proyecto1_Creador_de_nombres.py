6142991fcdce79d56c6bdef3e12c8b1dd669ac4a
Cristopher Hernandez <cristopher.hrz@hotmail.com>
2025-03-26 14:51:06 -0600

    Add files via upload

---
+++ Proyecto1_Creador_de_nombres.py
@@ -0,0 +1,5 @@
+print("Hola mundo");
+print("Hola \"Soy cristopher\" Bienvenido");
+print("Esta es una linea \nY este es otra linea");
+print("\tEsta es la tercera");
+print("Este es una barra  \\ invertida");

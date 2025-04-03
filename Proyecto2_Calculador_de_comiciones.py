bbf3434300e8b0ba000a87260e3584e8ce1d805a
Cristopher Hernandez <cristopher.hrz@hotmail.com>
2025-04-02 19:03:52 -0600

    Delete Proyecto2_Calculador_de_comiciones.py

--- Proyecto2_Calculador_de_comiciones.py
+++
@@ -1,9 +0,0 @@
-nombre = input("Nombre: ")
-apellido = input("Apellido: ")
-total_venta = float(input("Total de venta mensual: "))
-nombre_completo = nombre + " " + apellido
-porcentaje_comision = 13
-
-
-print("Nombre del vendedor: " + nombre_completo)
-print(f"Tu comision de este mes es: ${round(total_venta*porcentaje_comision/100,2)}")

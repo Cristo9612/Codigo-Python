c2a0b3ca29f6d1520af658af4305c4c17b861969
Cristopher Hernandez <cristopher.hrz@hotmail.com>
2025-04-02 19:03:39 -0600

    Delete Proyecto1_Creador_de_nombres.py

--- Proyecto1_Creador_de_nombres.py
+++
@@ -1,5 +0,0 @@
-print("Hola mundo");
-print("Hola \"Soy cristopher\" Bienvenido");
-print("Esta es una linea \nY este es otra linea");
-print("\tEsta es la tercera");
-print("Este es una barra  \\ invertida");
